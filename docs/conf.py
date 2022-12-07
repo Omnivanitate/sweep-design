c0c46c7a764e4119a863779ba075545b8a9e019f
Vladislav <serebraykov.vs@gmail.com>
2022-12-07 20:26:52 +0300

    add documentation

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sweep-design'
+copyright = '2022, Serebryakov Vladislav'
+author = 'Serebryakov Vladislav'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    'myst_nb',
+    # 'nbsphinx',
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = ['.rst', '.md']
+
+master_doc = 'index'
+
+autoapi_dirs = ["../sweep_design"]
+autoapi_python_class_content = 'both'
+
+myst_enable_extensions = ["dollarmath", "amsmath"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+```{include} ../README.md
+
+```
+
+```{toctree}
+:maxdepth: 1
+:hidden:
+
+tutorial.ipynb
+prepared_sweep.ipynb
+changelog.md
+contributing.md
+autoapi/index
+```
